85ab5aeab28f7e3f694b270275481f91e702dfd4
rené <rene.morales.sanchez@gmail.com>
2025-07-28 18:29:06 -0400

    yes

--- src/server/youtube.ts
+++ src/server/youtube.ts
@@ -4,9 +4,14 @@ import Innertube, { UniversalCache } from "youtubei.js";
 
 const ytCache = new UniversalCache(true);
 
+const cookie = await fs.readFileSync(
+  path.join(process.cwd(), "ytjs-cookies.txt"),
+  "utf8"
+);
+
 const innertube = await Innertube.create({
   cache: ytCache,
-  cookie: fs.readFileSync(path.join(process.cwd(), "ytjs-cookies.txt"), "utf8"),
+  cookie,
 });
 
 export default innertube;
